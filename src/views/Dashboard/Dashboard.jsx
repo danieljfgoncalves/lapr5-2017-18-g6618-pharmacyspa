8969571054748a9b86019c7c4972159370c4158b
FlavioRelvas <1140826@isep.ipp.pt>
2018-01-06 18:22:08 +0000

    added webgl example

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { React3 } from 'react'
 import { Grid, Col, Row } from 'react-bootstrap';
 // react component used to create charts
 import ChartistGraph from 'react-chartist';
@@ -16,6 +17,8 @@ import Card from 'components/Card/Card.jsx';
 import StatsCard from 'components/Card/StatsCard.jsx';
 import Tasks from 'components/Tasks/Tasks.jsx';
 
+import THREE from 'react-three-renderer'
+
 import {
     dataPie,
     dataSales,
@@ -26,40 +29,50 @@ import {
     responsiveBar,
     table_data
 } from 'variables/Variables.jsx';
+import Simple from '../../components/Boards/Simple';
 
 const colorScale = scaleLinear()
-.domain([0, 1, 6820])
-.range(["#E5E5E5", "#B2B2B2", "#000000"]);
+    .domain([0, 1, 6820])
+    .range(["#E5E5E5", "#B2B2B2", "#000000"]);
 
-class Dashboard extends Component{
-    createTableData(){
-        var tableRows = [];
-        for(var i = 0; i < table_data.length; i++){
-            tableRows.push(
-                <tr key={i}>
-                    <td>
-                        <div className="flag">
-                            <img src={table_data[i].flag} alt="us_flag"/>
-                        </div>
-                    </td>
-                    <td>{table_data[i].country}</td>
-                    <td className="text-right">{table_data[i].count}</td>
-                    <td className="text-right">{table_data[i].percentage}</td>
-                </tr>
-            );
-        }
-        return tableRows;
-    }
-    render(){
+class Dashboard extends Component {
+    render() {
         return (
             <div className="main-content">
-                
+                <Simple width="800" height="400" />
             </div>
         );
     }
 }
 
 export default Dashboard;
+
+/**createTableData() {
+    var tableRows = [];
+    for (var i = 0; i < table_data.length; i++) {
+                    tableRows.push(
+                        <tr key={i}>
+                            <td>
+                                <div className="flag">
+                                    <img src={table_data[i].flag} alt="us_flag" />
+                                </div>
+                            </td>
+                            <td>{table_data[i].country}</td>
+                            <td className="text-right">{table_data[i].count}</td>
+                            <td className="text-right">{table_data[i].percentage}</td>
+                        </tr>
+                    );
+                }
+    return tableRows;
+}
+render() {
+    return (
+        <div className="main-content">
+
+                </div>
+                );
+}*/
+
 /**<Grid fluid>
                     <Row>
                         <Col lg={3} sm={6}>
@@ -119,82 +132,82 @@ export default Dashboard;
                                             <ComposableMap style={{ width: "100%", height: "300px" }}>
                                                 <ZoomableGroup>
                                                     <Geographies geographyUrl="https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-50m.json">
-                                                    {(geographies, projection) => geographies.map(geography => {
-                                                        var style;
-                                                        switch (geography.id) {
-                                                            case "BRA":
-                                                            style={default: { fill: colorScale(550) }}
-                                                            break;
-                                                            case "USA":
-                                                            style={default: { fill: colorScale(2920) }}
-                                                            break;
-                                                            case "AUS":
-                                                            style={default: { fill: colorScale(760) }}
-                                                            break;
-                                                            case "DEU":
-                                                            style={default: { fill: colorScale(1300) }}
-                                                            break;
-                                                            case "GBR":
-                                                            style={default: { fill: colorScale(690) }}
-                                                            break;
-                                                            case "ROU":
-                                                            style={default: { fill: colorScale(600) }}
-                                                            break;
-                                                            default:
-                                                            style={default: { fill: colorScale(0) }}
-                                                            break;
-                                                        }
-                                                        return (
-                                                            <Geography
-                                                                key={ geography.id }
-                                                                geography={ geography }
-                                                                projection={ projection }
-                                                                onClick={ this.handleClick }
-                                                                style={style}
-                                                            />
-                                                        )
-                                                    })}
-                                                </Geographies>
-                                            </ZoomableGroup>
-                                        </ComposableMap>
-                                    </Col>
-                                </Row>
-                            }
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={4}>
-                        <Card
-                            title="Email Statistics"
-                            category="Last Campaign Performance"
-                            content={
-                                <ChartistGraph data={dataPie} type="Pie"/>
-                            }
-                            legend={
-                                <div>
-                                    <i className="fa fa-circle text-info"></i> Open
+                                                        {(geographies, projection) => geographies.map(geography => {
+                                                            var style;
+                                                            switch (geography.id) {
+                                                                case "BRA":
+                                                                    style = { default: { fill: colorScale(550) } }
+                                                                    break;
+                                                                case "USA":
+                                                                    style = { default: { fill: colorScale(2920) } }
+                                                                    break;
+                                                                case "AUS":
+                                                                    style = { default: { fill: colorScale(760) } }
+                                                                    break;
+                                                                case "DEU":
+                                                                    style = { default: { fill: colorScale(1300) } }
+                                                                    break;
+                                                                case "GBR":
+                                                                    style = { default: { fill: colorScale(690) } }
+                                                                    break;
+                                                                case "ROU":
+                                                                    style = { default: { fill: colorScale(600) } }
+                                                                    break;
+                                                                default:
+                                                                    style = { default: { fill: colorScale(0) } }
+                                                                    break;
+                                                            }
+                                                            return (
+                                                                <Geography
+                                                                    key={geography.id}
+                                                                    geography={geography}
+                                                                    projection={projection}
+                                                                    onClick={this.handleClick}
+                                                                    style={style}
+                                                                />
+                                                            )
+                                                        })}
+                                                    </Geographies>
+                                                </ZoomableGroup>
+                                            </ComposableMap>
+                                        </Col>
+                                    </Row>
+                                }
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={4}>
+                            <Card
+                                title="Email Statistics"
+                                category="Last Campaign Performance"
+                                content={
+                                    <ChartistGraph data={dataPie} type="Pie" />
+                                }
+                                legend={
+                                    <div>
+                                        <i className="fa fa-circle text-info"></i> Open
                                     <i className="fa fa-circle text-danger"></i> Bounce
                                     <i className="fa fa-circle text-warning"></i> Unsubscribe
                                 </div>
-                            }
-                            stats={
-                                <div>
-                                    <i className="fa fa-clock-o"></i> Campaign sent 2 days ago
+                                }
+                                stats={
+                                    <div>
+                                        <i className="fa fa-clock-o"></i> Campaign sent 2 days ago
                                 </div>
-                            }
-                        />
-                    </Col>
-                    <Col md={8}>
-                        <Card
-                            title="Users Behavior"
-                            category="24 Hours performance"
-                            content={
-                                <ChartistGraph
-                                    data={dataSales}
-                                    type="Line"
-                                    options={optionsSales}
-                                    responsiveOptions={responsiveSales}/>
+                                }
+                            />
+                        </Col>
+                        <Col md={8}>
+                            <Card
+                                title="Users Behavior"
+                                category="24 Hours performance"
+                                content={
+                                    <ChartistGraph
+                                        data={dataSales}
+                                        type="Line"
+                                        options={optionsSales}
+                                        responsiveOptions={responsiveSales} />
                                 }
                                 legend={
                                     <div>
