46793b0735daa75eb92826f26776c714b6b09556
Renato <1140822@isep.ipp.pt>
2018-01-09 19:22:47 +0000

    Prepare to implement dashboards

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -36,11 +36,26 @@ const colorScale = scaleLinear()
     .range(["#E5E5E5", "#B2B2B2", "#000000"]);
 
 class Dashboard extends Component {
+
     render() {
+        var data = {
+            labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+            series: [
+                [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+            ]
+        };
+
+        var options = {
+
+        };
+        var type = 'Bar'
+
         return (
-            <div className="main-content">
-                <Simple width="800" height="400" />
+
+            <div>
+                <ChartistGraph data={data} options={options} type={type} />
             </div>
+
         );
     }
 }
@@ -65,7 +80,7 @@ export default Dashboard;
                 }
     return tableRows;
 }
-render() {
+ render() {
     return (
         <div className="main-content">
 
@@ -74,50 +89,50 @@ render() {
 }*/
 
 /**<Grid fluid>
-                    <Row>
-                        <Col lg={3} sm={6}>
-                            <StatsCard
-                                bigIcon={<i className="pe-7s-server text-warning"></i>}
-                                statsText="Capacity"
-                                statsValue="105GB"
-                                statsIcon={<i className="fa fa-refresh"></i>}
-                                statsIconText="Updated now"
-                            />
-                        </Col>
-                        <Col lg={3} sm={6}>
-                            <StatsCard
-                                bigIcon={<i className="pe-7s-wallet text-success"></i>}
-                                statsText="Revenue"
-                                statsValue="$1,345"
-                                statsIcon={<i className="fa fa-calendar-o"></i>}
-                                statsIconText="Last day"
-                            />
-                        </Col>
-                        <Col lg={3} sm={6}>
-                            <StatsCard
-                                bigIcon={<i className="pe-7s-graph1 text-danger"></i>}
-                                statsText="Errors"
-                                statsValue="23"
-                                statsIcon={<i className="fa fa-clock-o"></i>}
-                                statsIconText="In the last hour"
-                            />
-                        </Col>
-                        <Col lg={3} sm={6}>
-                            <StatsCard
-                                bigIcon={<i className="fa fa-twitter text-info"></i>}
-                                statsText="Followers"
-                                statsValue="+45"
-                                statsIcon={<i className="fa fa-refresh"></i>}
-                                statsIconText="Updated now"
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md={12}>
-                            <Card
-                                title="Global Sales by Top Locations"
-                                category="All products that were shipped"
-                                content={
+ <Row>
+ <Col lg={3} sm={6}>
+ <StatsCard
+ bigIcon={<i className="pe-7s-server text-warning"></i>}
+ statsText="Capacity"
+ statsValue="105GB"
+ statsIcon={<i className="fa fa-refresh"></i>}
+ statsIconText="Updated now"
+ />
+ </Col>
+ <Col lg={3} sm={6}>
+ <StatsCard
+ bigIcon={<i className="pe-7s-wallet text-success"></i>}
+ statsText="Revenue"
+ statsValue="$1,345"
+ statsIcon={<i className="fa fa-calendar-o"></i>}
+ statsIconText="Last day"
+ />
+ </Col>
+ <Col lg={3} sm={6}>
+ <StatsCard
+ bigIcon={<i className="pe-7s-graph1 text-danger"></i>}
+ statsText="Errors"
+ statsValue="23"
+ statsIcon={<i className="fa fa-clock-o"></i>}
+ statsIconText="In the last hour"
+ />
+ </Col>
+ <Col lg={3} sm={6}>
+ <StatsCard
+ bigIcon={<i className="fa fa-twitter text-info"></i>}
+ statsText="Followers"
+ statsValue="+45"
+ statsIcon={<i className="fa fa-refresh"></i>}
+ statsIconText="Updated now"
+ />
+ </Col>
+ </Row>
+ <Row>
+ <Col md={12}>
+ <Card
+ title="Global Sales by Top Locations"
+ category="All products that were shipped"
+ content={
                                     <Row>
                                         <Col md={5}>
                                             <div className="table-responsive">
@@ -173,63 +188,63 @@ render() {
                                         </Col>
                                     </Row>
                                 }
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md={4}>
-                            <Card
-                                title="Email Statistics"
-                                category="Last Campaign Performance"
-                                content={
+ />
+ </Col>
+ </Row>
+ <Row>
+ <Col md={4}>
+ <Card
+ title="Email Statistics"
+ category="Last Campaign Performance"
+ content={
                                     <ChartistGraph data={dataPie} type="Pie" />
                                 }
-                                legend={
+ legend={
                                     <div>
                                         <i className="fa fa-circle text-info"></i> Open
                                     <i className="fa fa-circle text-danger"></i> Bounce
                                     <i className="fa fa-circle text-warning"></i> Unsubscribe
                                 </div>
                                 }
-                                stats={
+ stats={
                                     <div>
                                         <i className="fa fa-clock-o"></i> Campaign sent 2 days ago
                                 </div>
                                 }
-                            />
-                        </Col>
-                        <Col md={8}>
-                            <Card
-                                title="Users Behavior"
-                                category="24 Hours performance"
-                                content={
+ />
+ </Col>
+ <Col md={8}>
+ <Card
+ title="Users Behavior"
+ category="24 Hours performance"
+ content={
                                     <ChartistGraph
                                         data={dataSales}
                                         type="Line"
                                         options={optionsSales}
                                         responsiveOptions={responsiveSales} />
                                 }
-                                legend={
+ legend={
                                     <div>
                                         <i className="fa fa-circle text-info"></i> Open
                                         <i className="fa fa-circle text-danger"></i> Click
                                         <i className="fa fa-circle text-warning"></i> Click Second Time
                                     </div>
                                 }
-                                stats={
+ stats={
                                     <div>
                                         <i className="fa fa-history"></i> Updated 3 minutes ago
                                     </div>
                                 }
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md={6}>
-                            <Card
-                                title="2014 Sales"
-                                category="All products including Taxes"
-                                content={
+ />
+ </Col>
+ </Row>
+ <Row>
+ <Col md={6}>
+ <Card
+ title="2014 Sales"
+ category="All products including Taxes"
+ content={
                                     <ChartistGraph
                                         data={dataBar}
                                         type="Bar"
@@ -237,34 +252,34 @@ render() {
                                         responsiveOptions={responsiveBar}
                                     />
                                 }
-                                legend={
+ legend={
                                     <div>
                                         <i className="fa fa-circle text-info"></i> Tesla Model S
                                         <i className="fa fa-circle text-danger"></i> BMW 5 Series
                                     </div>
                                 }
-                                stats={
+ stats={
                                     <div>
                                         <i className="fa fa-check"></i> Data information certified
                                     </div>
                                 }
-                            />
-                        </Col>
-                        <Col md={6}>
-                            <Card
-                                title="Tasks"
-                                category="Backend development"
-                                content={
+ />
+ </Col>
+ <Col md={6}>
+ <Card
+ title="Tasks"
+ category="Backend development"
+ content={
                                     <table className="table">
                                         <Tasks />
                                     </table>
                                 }
-                                stats={
+ stats={
                                     <div>
                                         <i className="fa fa-history"></i> Updated 3 minutes ago
                                     </div>
                                 }
-                            />
-                        </Col>
-                    </Row>
-                </Grid> */
+ />
+ </Col>
+ </Row>
+ </Grid> */
